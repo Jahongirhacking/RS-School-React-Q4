6179321988ebe6988b5a10c143835a189d9876d3
Jahongirhacking <Jahongirhacking@gmail.com>
2023-11-08 13:39:33 +0500

    footer added

---
+++ src/pages/Footer/index.tsx
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">&copy;Bulalar Team - Jahongir Hayitov 2023</footer>
+    )
+}
+
+export default Footer
