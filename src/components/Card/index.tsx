64131e653d09c6588f9e351e3a086837f0c483e5
Jahongirhacking <Jahongirhacking@gmail.com>
2023-11-08 13:38:15 +0500

    details button link updated

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,29 @@
+// Routing
 import { Link, useSearchParams } from 'react-router-dom';
+// Types
 import { IPokemonLocal } from './interface';
-import { Routes as RoutePaths } from '../../routes';
+// Styles
 import './style.scss';
+// Images
+import pokemonImage from '../../assets/PokemonImage.jpg'
+// API
+import { handleImageError } from '../../services/imageError';
+import { getDetailsParam } from '../../services/searchParam';
 
 const Card = (props: IPokemonLocal) => {
   const { pokemonName, imgUrl, abilities, height, id } = props;
 
   const [searchParams] = useSearchParams();
+  const linkTo = getDetailsParam(id, searchParams)
 
   return (
     <article className="card">
       <div className='card__img-container'>
-        <img src={imgUrl} alt="pokemon image" />
+        <img
+          src={imgUrl}
+          alt="pokemon image"
+          onError={(e) => { handleImageError(e, pokemonImage) }}
+        />
       </div>
       <section className='info'>
         <h2>{pokemonName}</h2>
@@ -23,7 +35,7 @@ const Card = (props: IPokemonLocal) => {
         </p>
         <Link
           className='card__btn btn'
-          to={`${RoutePaths.DETAILS}/${id}/?page=${searchParams.get('page')}`}>
+          to={linkTo}>
           Details
         </Link>
       </section>
