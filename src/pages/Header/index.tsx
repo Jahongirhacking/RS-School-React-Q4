384112cb4958ae4149026782694286455d7a6c9e
Jahongirhacking <Jahongirhacking@gmail.com>
2023-11-12 20:52:13 +0500

    pagination component added

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,104 +1,44 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 // Components
 import Input from '../../components/Input';
-import Button from '../../components/Button';
+import LogoImage from "../../assets/logo.png"
 // Types
 import { ISearchContext } from '../../@types/app';
-// API
-import { LIMIT, fetchData } from '../../services/api';
 // Styles
 import './style.scss';
-// Routing
-import { useSearchParams } from 'react-router-dom';
 // FontAwesome Icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTriangleExclamation, faArrowRight, faArrowLeft, faSearch, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp, faSearch } from '@fortawesome/free-solid-svg-icons';
 // Context API
 import { SearchContext } from '../../contexts/SearchContext';
+import Pagination from '../../components/Pagination';
 
 const Header = () => {
+  const { searchedKey, handleSearchedKey } = useContext(SearchContext) as ISearchContext;
+  const [showPagination, setShowPagination] = useState(false);
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { searchedKey, handleSearchedKey, setIsPending, setIsError } = useContext(SearchContext) as ISearchContext;
-
-  const pageNumber = Number(searchParams.get("page")) || 0;
-  const limitNumber = Number(searchParams.get("limit")) || LIMIT;
-
-  const handlePagination = (callbackFunc: (value: number) => number) => {
-    setSearchParams((param) => {
-      const page = callbackFunc(Number(param.get("page")) || 0);
-      localStorage.setItem("pageNumber", `${page}`);
-      param.set("page", `${page}`);
-      handleSearchedKey("");
-      return param;
-    });
+  const togglePagination = () => {
+    setShowPagination(showPagination => !showPagination);
   }
-
-  const handlePrevPagination = () => {
-    handlePagination((value: number) => value - 1);
-  };
-
-  const handleNextPagination = () => {
-    handlePagination((value: number) => value + 1);
-  };
-
-  const handleLimitChange = (value: string) => {
-    localStorage.setItem('limitNumber', `${value}`)
-    setSearchParams((params) => {
-      params.set("limit", `${value}`);
-      return params;
-    })
-  }
-
-  const fetchFakeData = async () => {
-    try {
-      setIsError(false);
-      setIsPending(true);
-      // Pending Begins
-      fetchData('somefakedata.hack/api', setIsError);
-    } catch (err) {
-      setIsError(true);
-      console.error(err);
-    } finally {
-      setIsPending(false);
-    }
-  };
 
   return (
     <header className="header">
-      <nav className="nav header__search">
-        <Input
-          className='input-name'
-          searchedKey={searchedKey}
-          setSearchedKey={handleSearchedKey}
-          placeholder={'Pokemon Name...'}
-          type={'text'}
-          btnContent={<FontAwesomeIcon icon={faSearch} />}
-        />
-        <Input
-          className='input-limit'
-          searchedKey={searchParams.get('limit') || `${LIMIT}`}
-          setSearchedKey={handleLimitChange}
-          placeholder={'Limit'}
-          type={'number'}
-          btnContent={<FontAwesomeIcon icon={faCheck} />}
-        />
-        <section className="btn-container">
-          {(pageNumber) > 0 && (
-            <Button handleClick={handlePrevPagination}>
-              <FontAwesomeIcon icon={faArrowLeft} />
-            </Button>
-          )}
-          {(pageNumber * limitNumber) < 1000 && (
-            <Button handleClick={handleNextPagination}>
-              <FontAwesomeIcon icon={faArrowRight} />
-            </Button>
-          )}
-          <Button handleClick={fetchFakeData} btnName="danger-bg">
-
-            <FontAwesomeIcon icon={faTriangleExclamation} />
-          </Button>
-        </section>
+      <nav className="nav">
+        <img className='header__logo' src={LogoImage} alt="logo-image" />
+        <form className='header__search' onSubmit={(e) => { e.preventDefault() }}>
+          <Input
+            className='input-name'
+            searchedKey={searchedKey}
+            setSearchedKey={handleSearchedKey}
+            placeholder={'Pokemon Name...'}
+            type={'text'}
+            btnContent={<FontAwesomeIcon icon={faSearch} />}
+          />
+          <button className='toggle-btn btn' onClick={togglePagination}>
+            <FontAwesomeIcon icon={showPagination ? faAngleUp : faAngleDown} />
+          </button>
+          {showPagination && <Pagination />}
+        </form>
       </nav>
     </header>
   );
